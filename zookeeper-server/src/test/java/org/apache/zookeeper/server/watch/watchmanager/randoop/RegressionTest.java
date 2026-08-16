e311956e89685cae8c2c8703deb5634176e73447
Salvatore Casolaro <casolaro.salvatore24@gmail.com>
2026-08-16 16:47:18 +0200

    Modified package in order to run Randoop&Evosuite Tests for C2

--- zookeeper-server/src/test/java/org/apache/zookeeper/server/watch/watchmanager/randoop/RegressionTest.java
+++ zookeeper-server/src/test/java/org/apache/zookeeper/server/watch/watchmanager/randoop/RegressionTest.java
@@ -1,10 +1,11 @@
-package org.apache.zookeeper.server.watch;
+package org.apache.zookeeper.server.watch.watchmanager.randoop;
 
+import org.apache.zookeeper.server.watch.RegressionTest0;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ org.apache.zookeeper.server.watch.RegressionTest0.class })
+@Suite.SuiteClasses({ RegressionTest0.class })
 public class RegressionTest {
 }
 
