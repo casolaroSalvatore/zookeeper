06842804ba7f196fc697204253c395d2076f48c4
casolaroSalvatore <150945794+casolaroSalvatore@users.noreply.github.com>
2026-09-01 09:27:14 +0200

    Upload test variant C0 SnapshotComparer

---
+++ zookeeper-server/src/test/java/org/apache/zookeeper/server/snapshotcomparer/blackbox/variant_c0/randoop/beforefacade/RegressionTestBeforeFacadeTest.java
@@ -0,0 +1,10 @@
+package org.apache.zookeeper.server.snapshotcomparer.randoop.beforefacade;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RegressionTestBeforeFacadeTest0.class })
+public class RegressionTestBeforeFacadeTest {
+}
+
